b067e382dd2e646b130e6eed7b79a64bdcc376f9
John Maicon <john.pereira@localiza.com>
2025-03-31 17:39:06 -0300

    add: page about

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,14 @@
+export default async function AboutPage() {
+    return (
+        <div className="w-1/2 bg-slate-100 min-h-screen flex flex-col m-auto p-4 rounded">
+            <h1>About Us</h1>
+            <p>Our project was created with the aim of connecting freelancers with companies looking for skilled
+                professionals. We provide a platform where job seekers can find opportunities that match their skills
+                and preferences.</p>
+            <p>Our mission is to simplify the job search process and help companies find the right talent for their
+                needs. We believe in the power of technology to bring people together and create meaningful work
+                relationships.</p>
+            <p>Thank you for visiting our platform. We hope you find the perfect job or candidate here!</p>
+        </div>
+    );
+}
