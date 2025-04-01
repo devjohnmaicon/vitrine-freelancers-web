0e3bd51bbdc22034e20a6bdf4ee339800808f726
John Maicon <john.pereira@localiza.com>
2025-04-01 17:00:31 -0300

    add: modal new job (template)

--- src/app/minhas-vagas/page.tsx
+++ src/app/minhas-vagas/page.tsx
@@ -1,5 +1,7 @@
 import CardComponent from "@/components/CardComponent";
 import {JobType} from "@/types/JobType";
+import {UserPlus} from "lucide-react";
+import NewJobComponent from "@/components/NewJobComponent";
 
 
 export default async function MyJobsPage() {
@@ -16,14 +18,21 @@ export default async function MyJobsPage() {
 
 
     return (
-        <div className="w-1/2 bg-slate-100 min-h-screen  max-w-screen-lg flex flex-col gap-3 m-auto p-4 rounded">
-            {
-                myJobs.map((job) => {
-                    return (
-                        <CardComponent key={job.id} data={job} showEditButtons={true}/>
-                    )
-                })
-            }
+        <div className="w-1/2 border min-h-screen max-w-screen-lg flex flex-col m-auto p-2 rounded-lg my-2">
+            <div>
+                {/*<button className="btn btn-primary"><UserPlus />  Cadastrar </button>*/}
+                <NewJobComponent/>
+            </div>
+            <div className='flex flex-col gap-3 mt-1'>
+                {
+                    myJobs.map((job) => {
+                        return (
+                            <CardComponent key={job.id} data={job} showEditButtons={true}/>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     );
 }
