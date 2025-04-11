da4c538a914d4d174395e60577a33375a2fb9859
John Maicon <john.pereira@localiza.com>
2025-04-11 16:27:46 -0300

    add: create contact page with form for user inquiries

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,14 +1,67 @@
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
+import {Button} from "@/components/ui/button";
+
 export default async function AboutPage() {
     return (
-        <div className="w-1/2 bg-slate-100 min-h-screen flex flex-col m-auto p-4 rounded">
-            <h1>About Us</h1>
-            <p>Our project was created with the aim of connecting freelancers with companies looking for skilled
-                professionals. We provide a platform where job seekers can find opportunities that match their skills
-                and preferences.</p>
-            <p>Our mission is to simplify the job search process and help companies find the right talent for their
-                needs. We believe in the power of technology to bring people together and create meaningful work
-                relationships.</p>
-            <p>Thank you for visiting our platform. We hope you find the perfect job or candidate here!</p>
-        </div>
+        <section className="py-32">
+            <div className="container">
+                <div className="mx-auto flex max-w-screen-xl flex-col justify-between gap-10 lg:flex-row lg:gap-20">
+                    <div className="mx-auto flex max-w-sm flex-col justify-between gap-10">
+                        <div className="text-center lg:text-left">
+                            <h1 className="mb-2 text-5xl font-semibold lg:mb-1 lg:text-6xl">
+                                Fale Conosco
+                            </h1>
+                            <p className="text-muted-foreground">
+                                Estamos disponíveis para perguntas, feedback ou oportunidades de colaboração. Deixe-nos saber como podemos ajudar!
+                            </p>
+                        </div>
+                        <div className="mx-auto w-fit lg:mx-0">
+                            <h3 className="mb-6 text-center text-2xl font-semibold lg:text-left">
+                                Informações de Contato
+                            </h3>
+                            <ul className="ml-4 list-disc">
+                                <li>
+                                    <span className="font-bold">Phone: </span>
+                                    (123) 34567890
+                                </li>
+                                <li>
+                                    <span className="font-bold">Email: </span>
+                                    <a href="" className="underline">
+                                        your-email@example.com
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="mx-auto flex max-w-screen-md flex-col gap-6 rounded-lg border p-10">
+                        <div className="flex gap-4">
+                            <div className="grid w-full items-center gap-1.5">
+                                <Label htmlFor="firstname">First Name</Label>
+                                <Input type="text" id="firstname" placeholder="First Name" />
+                            </div>
+                            <div className="grid w-full items-center gap-1.5">
+                                <Label htmlFor="lastname">Last Name</Label>
+                                <Input type="text" id="lastname" placeholder="Last Name" />
+                            </div>
+                        </div>
+                        <div className="grid w-full items-center gap-1.5">
+                            <Label htmlFor="email">Email</Label>
+                            <Input type="email" id="email" placeholder="Email" />
+                        </div>
+                        <div className="grid w-full items-center gap-1.5">
+                            <Label htmlFor="subject">Subject</Label>
+                            <Input type="text" id="subject" placeholder="Subject" />
+                        </div>
+                        <div className="grid w-full gap-1.5">
+                            <Label htmlFor="message">Message</Label>
+                            <Textarea placeholder="Type your message here." id="message" />
+                        </div>
+                        <Button className="w-full">Send Message</Button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
