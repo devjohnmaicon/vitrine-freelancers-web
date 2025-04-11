e6268b03398087769273bbc091576b514b480ed0
John Maicon <john.pereira@localiza.com>
2025-04-11 16:29:23 -0300

    refactor: overhaul header component with new navigation and mobile support

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,88 +1,123 @@
-import { signOut } from "next-auth/react";
-import { auth } from "../../auth";
-import BtnLogout from "./BtnLogout";
-import Link from "next/link";
+"use client";
 
-export async function HeaderComponent() {
-  const session = await auth();
-  console.log("session", session);
+import {MenuIcon} from "lucide-react";
 
-  return (
-    <header className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/vagas">Vagas</a>
-            </li>
-            {session?.user && (
-              <li>
-                <a href="/minhas-vagas">Minhas Vagas</a>
-              </li>
-            )}
-            <li>
-              <a href="/sobre">Sobre</a>
-            </li>
-          </ul>
-        </div>
-        <a href="/" className="btn btn-ghost text-xl">
-          daisyUI
-        </a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/vagas">Vagas</a>
-          </li>
-          {session?.user && (
-            <li>
-              <a href="/minhas-vagas">Minhas Vagas</a>
-            </li>
-          )}
-          <li>
-            <a href="/sobre">Sobre</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        {!session?.user && (
-          <div className="flex gap-3">
-            <Link href="/login" className="btn">
-              Login
-            </Link>
-            <Link href="/register" className="btn btn-outlin">
-              Registrar-se
-            </Link>
-          </div>
-        )}
+import {Button} from "@/components/ui/button";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import {Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger,} from "@/components/ui/sheet";
+import {useRouter} from "next/navigation";
 
-        {session?.user && <BtnLogout />}
-      </div>
-    </header>
-  );
-}
+export default function HeaderComponent() {
+    const router = useRouter();
+    return (
+        <header className="py-4 border-b">
+            <div className="container m-auto">
+                <nav className="flex items-center justify-between">
+                    <a
+                        href="/"
+                        className="flex items-center gap-2"
+                    >
+                        <img
+                            src="https://shadcnblocks.com/images/block/logos/shadcnblockscom-icon.svg"
+                            className="max-h-8"
+                            alt="Shadcn UI Navbar"
+                        />
+                        <span className="text-lg font-semibold tracking-tighter">
+              Shadcnblocks.com
+            </span>
+                    </a>
+                    <NavigationMenu className="hidden lg:block">
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink
+                                    href="/"
+                                    className={`${navigationMenuTriggerStyle()}  rounded-none border-b-4 border-zinc-700`}
+                                >
+                                    Início
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink
+                                    href="/vagas"
+                                    className={navigationMenuTriggerStyle()}
+                                >
+                                    Vagas
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink
+                                    href="/minhas-vagas"
+                                    className={navigationMenuTriggerStyle()}
+                                >
+                                    Minhas vagas
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink
+                                    href="/sobre"
+                                    className={navigationMenuTriggerStyle()}
+                                >
+                                    Sobre
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                    <div className="hidden items-center gap-4 lg:flex">
+                        <Button variant="outline" onClick={() => router.push("/login")}>Login</Button>
+                        <Button onClick={() => router.push("/register")}>Registre-se</Button>
+                    </div>
+                    <Sheet>
+                        <SheetTrigger asChild className="lg:hidden">
+                            <Button variant="outline" size="icon">
+                                <MenuIcon className="h-4 w-4"/>
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="top" className="max-h-screen overflow-auto">
+                            <SheetHeader>
+                                <SheetTitle>
+                                    <a
+                                        href="https://www.shadcnblocks.com"
+                                        className="flex items-center gap-2"
+                                    >
+                                        <img
+                                            src="https://shadcnblocks.com/images/block/logos/shadcnblockscom-icon.svg"
+                                            className="max-h-8"
+                                            alt="Shadcn UI Navbar"
+                                        />
+                                        <span className="text-lg font-semibold tracking-tighter">
+                      Shadcnblocks.com
+                    </span>
+                                    </a>
+                                </SheetTitle>
+                            </SheetHeader>
+                            <div className="flex flex-col p-4">
+                                <div className="flex flex-col gap-6">
+                                    <a href="#" className="font-medium">
+                                        Templates
+                                    </a>
+                                    <a href="#" className="font-medium">
+                                        Blog
+                                    </a>
+                                    <a href="#" className="font-medium">
+                                        Pricing
+                                    </a>
+                                </div>
+                                <div className="mt-6 flex flex-col gap-4">
+                                    <Button variant="outline">Sign in</Button>
+                                    <Button>Start for free</Button>
+                                </div>
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
