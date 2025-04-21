1ee66c4f4d2e155785f58f4c99355254da95a2d5
John Maicon <john.pereira@localiza.com>
2025-04-21 16:05:39 -0300

    feat: add Montserrat font and improve layout with font preloading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,11 @@ export default function RootLayout({children}: Readonly<{
 }>) {
     return (
         <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+        </head>
         <body>
         <div className="h-screen ">
             <HeaderComponent/>
