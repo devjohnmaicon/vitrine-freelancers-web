3d10fea90d3f24a3efc10fcb3ada8e69ddf8b306
John Maicon <john.pereira@localiza.com>
2025-10-03 19:35:09 -0300

    feat: enhance header navigation with dynamic styling for active links

--- src/components/header/HeaderNavigation.tsx
+++ src/components/header/HeaderNavigation.tsx
@@ -1,16 +1,30 @@
+'use client';
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
-export default async function HeaderNavigation({ session }: { session: any }) {
+
+export default  function HeaderNavigation({ session }: { session: any }) {
+    let pathname = usePathname();
+    if (pathname === '/vagas/minhas-vagas'){
+        pathname = '/minhas-vagas'
+    }
 
     return (
         <nav className="hidden lg:block">
-             <Link href="/" className={`${navigationMenuTriggerStyle()}`}> Início </Link>
+            <Link href="/" 
+                className={`${navigationMenuTriggerStyle()} ${pathname === '/' ? 'bg-zinc-50 border-b-4 border-zinc-400' : ''}`} 
+            > Início </Link>
             <Link href="/vagas"
-                  className={`${navigationMenuTriggerStyle()}`}> Vagas </Link>
-            {session && <Link href="/vagas/minhas-vagas" className={navigationMenuTriggerStyle()}> Minhas vagas </Link>}
-            <Link href="/sobre" className={navigationMenuTriggerStyle()}> Sobre </Link>
-            <Link href="/contato" className={navigationMenuTriggerStyle()}> Contato </Link>
+                className={`${navigationMenuTriggerStyle()} ${pathname === '/vagas' ? 'bg-zinc-50 border-b-4 border-zinc-400' : ''}`}> Vagas </Link>
+
+            {session && <Link href="/vagas/minhas-vagas" 
+                className={`${navigationMenuTriggerStyle()} ${pathname === '/minhas-vagas' ? 'bg-zinc-50 border-b-4 border-zinc-400' : ''}`}> Minhas vagas </Link>}
+            <Link href="/sobre" 
+                className={`${navigationMenuTriggerStyle()} ${pathname === '/sobre' ? 'bg-zinc-50 border-b-4 border-zinc-400' : ''}`}> Sobre </Link>
+            <Link href="/contato" 
+                className={`${navigationMenuTriggerStyle()} ${pathname === '/contato' ? 'bg-zinc-50 border-b-4 border-zinc-400' : ''}`}> Contato </Link>
         </nav>
     )
 }
